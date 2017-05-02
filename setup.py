86db0a949b19e356e902a8d7e0034f890cb092cb
Jordan Enterkin <jordan.a.enterkin@gmail.com>
2017-05-01 22:54:49 -0400

    add setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'readme.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='selenium_page_elements',
+    version='0.1.2',
+    description='A small library for simplifying page objects.',
+    long_description=long_description,
+
+    url='https://git.snippr.io/jenterkin/selenium-page-elements',
+
+    author='Jordan Enterkin',
+    author_email='jordan.a.enterkin@gmail.com',
+
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Testing',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='selenium page objects',
+
+    packages=['page_elements'],
+
+    install_requires=['selenium>=3.4.0'],
+    extras_require={
+        'test': ['docker>=2.2.1', 'pytest>=3.0.7']
+    }
+)
